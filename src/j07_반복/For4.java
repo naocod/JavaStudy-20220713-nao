0b4d8d7de55de70e12f7e2240e15f5a7748c592e
naocod <8162857@naver.com>
2022-07-21 12:51:29 +0900

    챕터 7. 반복

--- src/j07_반복/For4.java
+++ src/j07_반복/For4.java
@@ -14,17 +14,16 @@ public class For4 {
 				int index = 0;
 				String temp = address.substring(0, i);
 				if(temp.contains(" ")) { // 문자열 중에서 조건 문자열이 포함되어 있으면 > true
-					
-				}
-				index = temp.lastIndexOf(" ") + 1; // 뒤에서부터 조건 문자열을 찾음 > 뒤에서부터 잘라서 동부터 시작함
-				System.out.println(temp.substring(index, i)); // index부터 i전까지 자름
-			}else {
-				System.out.println(temp.substring(0, i));
-			}
+					index = temp.lastIndexOf(" ") + 1; // 뒤에서부터 조건 문자열을 찾음 > 뒤에서부터 잘라서 동부터 시작함
+					System.out.println(temp.substring(index, i)); // index부터 i전까지 자름
+				}else {
+					System.out.println(temp.substring(0, i));
+				}	
 			
-		}
-		if(i == address.length() - 1) {
-			System.out.println(address.substring(address.lastIndexOf(" ") + 1));
+			}
+			if(i == address.length() - 1) {
+				System.out.println(address.substring(address.lastIndexOf(" ") + 1));
+			}
 		}
 		
 	}
