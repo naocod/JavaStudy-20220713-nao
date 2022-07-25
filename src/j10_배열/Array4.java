312839db8fdb2681964739baaf1cd2f70c6cbef5
naocod <8162857@naver.com>
2022-07-25 12:44:26 +0900

    챕터 10. 배열

---
+++ src/j10_배열/Array4.java
@@ -0,0 +1,15 @@
+package j10_배열;
+
+public class Array4 {
+
+	public static void main(String[] args) {
+		int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+		
+		// name.length; > 메소드를 통해서 문자열의 길이를 반환
+		
+		for(int i = 0; i < numbers.length; i++) { // numbers.length > 변수
+			System.out.println(numbers[i]);
+		}
+	}
+
+}
---
+++ src/j10_배열/Arrays/Arrays5.java
@@ -0,0 +1,38 @@
+package j10_배열.Arrays;
+
+import j09_클래스.접근지정자.student.Student;
+
+public class Arrays5 {
+
+	public static void main(String[] args) {
+		
+		int[] nums = new int[5];
+		
+		
+		for(int i = 0; i < nums.length; i++) {
+			System.out.println(nums[i]);
+		}
+		
+		
+		System.out.println("------------------------------");
+		
+		
+		Student[] students = new Student[5];
+		for(int i = 0; i < students.length; i++) {
+			students[i] = new Student();
+		}
+//		students[0] = new Student();
+//		students[0].setName("김준일");
+//		System.out.println(students[0].getName());
+//		students[1].setName("김준이");
+		
+		for(int i = 0; i < students.length; i++) {
+			System.out.println(students[i]);
+		}
+		
+		
+		
+		
+	}
+
+}
